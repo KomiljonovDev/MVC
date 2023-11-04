74ee3e304b4e383900ce3fed42329346a9b18dcc
KomiljonovDev <uzokdeveloper@gmail.com>
2023-11-04 21:44:34 +0500

    update

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -1,3 +1,21 @@
 <?php
+    namespace Controllers;
+    class UserController{
+        public static function func1 () {
+            return view('welcome',[
+                'name'=>'Obidjon',
+                'lastname'=>'Komiljonov'
+            ]);
+        }
+        public static function func2 () {
+            echo "test-2";
+            return true;
+        }
+    }
 
+
+    function view (string $view, array $arr = []) {
+        extract($arr);
+        include '../views/' . $view . '.php';
+    }
 ?>
