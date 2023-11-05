9d8d950c7aa5c6ca5292e4e1731607e3645626e4
Komiljonovdev <uzokdeveloper@gmail.com>
2023-11-05 11:43:21 +0500

    update

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -8,6 +8,22 @@ final class Router {
         self::$routes = array_merge(self::$routes, [Request::scriptName() . $url => ['Controller'=>$Controller[0], 'Method'=>$Controller[1], 'type'=>'GET']]);
         return self::$routes;
     }
+    public static function post ($url, $Controller) {
+        self::$routes = array_merge(self::$routes, [Request::scriptName() . $url => ['Controller'=>$Controller[0], 'Method'=>$Controller[1], 'type'=>'POST']]);
+        return self::$routes;
+    }
+    public static function put ($url, $Controller) {
+        self::$routes = array_merge(self::$routes, [Request::scriptName() . $url => ['Controller'=>$Controller[0], 'Method'=>$Controller[1], 'type'=>'PUT']]);
+        return self::$routes;
+    }
+    public static function patch ($url, $Controller) {
+        self::$routes = array_merge(self::$routes, [Request::scriptName() . $url => ['Controller'=>$Controller[0], 'Method'=>$Controller[1], 'type'=>'PATCH']]);
+        return self::$routes;
+    }
+    public static function delete ($url, $Controller) {
+        self::$routes = array_merge(self::$routes, [Request::scriptName() . $url => ['Controller'=>$Controller[0], 'Method'=>$Controller[1], 'type'=>'DELETE']]);
+        return self::$routes;
+    }
     public static function routeAll () {
         return self::$routes;
     }
