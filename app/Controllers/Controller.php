16de5531606f53104ee6ad7fbb38848974a57bf2
Komiljonovdev <uzokdeveloper@gmail.com>
2023-12-16 17:54:12 +0500

    update

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,21 +6,20 @@ use app\Core\Model\Model;
 use app\Models\User;
 
 
-
 class Controller {
     public static function welcome ():void {
-        return view('welcome',[
+        view('welcome',[
             'message'=>'Assalomu alaykum'
         ]);
     }
     public static function helloWorld ():void {
-        return view('hello-world',[
+        view('hello-world',[
             'message'=>'Assalomu alaykum'
         ]);
     }
 
     public static function  login() {
-        return view('login');
+        view('login');
     }
 
 }
