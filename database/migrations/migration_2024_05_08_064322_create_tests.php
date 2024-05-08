25fe089041e31bf351abe288d3264d58ded0e539
Komiljonovdev <uzokdeveloper@gmail.com>
2024-05-08 12:06:18 +0500

    update

--- database/migrations/migration_2024_05_08_064322_create_tests.php
+++ database/migrations/migration_2024_05_08_064322_create_tests.php
@@ -1,18 +1,19 @@
 <?php
 use \App\Core\database\Migration;
 class tests extends Migration {
-    public function up(){
+    public static function up(){
         parent::create('tests',[
            'id'=>'INT PRIMARY KEY',
             'created_at'=>'DATETIME',
             'deleted_at'=>'DATETIME'
         ]);
     }
-    public function drop(){
+    public static function drop(){
         if (parent::tabeleExist('tests')){
             parent::drop('tests');
         }
     }
 }
+
 
 ?>
